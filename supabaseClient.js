584ba5759142917570b4746fca879174effd7f2a
arun2003k <154778408+arun2003k@users.noreply.github.com>
2025-04-16 09:54:46 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,3 +4,4 @@ const supabaseUrl = "https://wnovshdbuptvlpjdmebs.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indub3ZzaGRidXB0dmxwamRtZWJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyOTkwMjQsImV4cCI6MjA1OTg3NTAyNH0.XGnuY84t-gAckLF4kas-e9lrslpMcuuwytyQsm6QU1g";
 
 export const supabase= createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
